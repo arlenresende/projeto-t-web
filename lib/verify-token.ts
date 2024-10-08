85453e43a3963e72e4de6d41597b3667d3ab6959
arlenresende <arlen.resende@blip.ai>
2024-10-08 14:30:30 -0300

    fix: fix eslint project

--- lib/verify-token.ts
+++ lib/verify-token.ts
@@ -1,15 +1,6 @@
-import { jwtVerify } from 'jose'
-
 export default async function verifyToken(token: string): Promise<boolean> {
   if (!token) return false
   try {
-    // await jwtVerify(
-    //   token,
-    //   new TextEncoder().encode(process.env.NEXT_PUBLIC_JWT_SECRET),
-    //   {
-    //     algorithms: ['HS256'],
-    //   },
-    // )
     return true
   } catch (error: unknown) {
     console.error(error)
